3f0648f5f2e3c60400cfac118d149ec7a06716a4
Saket Khare <saketkhare2000@gmail.com>
2021-08-30 03:43:08 +0530

    Ft: CSS Grid

---
+++ test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    // char Saket, Shubham, Dhruv, aryan, kratika, arjun, ambika, arya, tushar, megha, simran, samarth, prashant, supriti, robin, himanshi;
+    char heavy_drivers[100][100] = {"Saket", "Shubham", "Dhruv", "aryan", "kratika", "arjun", "ambika", "arya", "tushar", "megha", "simran", "samarth", "prashant", "supriti", "robin", "himanshi"};
+    printf("2:Members of heavy drivers are %c\n", heavy_drivers[1]);
+    printf("1:Members of heavy drivers are %s\n", heavy_drivers[0]);
+    printf("3:Members of heavy drivers are %s\n", heavy_drivers[2]);
+    printf("4:Members of heavy drivers are %s\n", heavy_drivers[3]);
+    printf("5:Members of heavy drivers are %s\n", heavy_drivers[4]);
+    printf("6:Members of heavy drivers are %s\n", heavy_drivers[5]);
+    printf("7:Members of heavy drivers are %s\n", heavy_drivers[6]);
+    printf("8:Members of heavy drivers are %s\n", heavy_drivers[7]);
+    printf("9:Members of heavy drivers are %s\n", heavy_drivers[8]);
+    printf("10:Members of heavy drivers are %s\n", heavy_drivers[9]);
+    printf("11:Members of heavy drivers are %s\n", heavy_drivers[10]);
+    printf("12:Members of heavy drivers are %s\n", heavy_drivers[11]);
+    printf("13:Members of heavy drivers are %s\n", heavy_drivers[12]);
+    printf("14:Members of heavy drivers are %s\n", heavy_drivers[13]);
+    printf("15:Members of heavy drivers are %s\n", heavy_drivers[14]);
+    printf("16:Members of heavy drivers are %s\n", heavy_drivers[15]);
+    return 0;
+}
